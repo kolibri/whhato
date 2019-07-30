e6d32865afbe40f32fc8bfb9d2d5745f697d8888
Lukas Sadzik <lukas.sadzik@sensiolabs.de>
2019-07-30 14:21:39 +0200

    Something working oauth stuff

--- project/config/bundles.php
+++ project/config/bundles.php
@@ -4,4 +4,8 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
--- project/src/Controller/SlackController.php
+++ project/src/Controller/SlackController.php
@@ -4,40 +4,46 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use KnpU\OAuth2ClientBundle\Client\Provider\SlackClient;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class SlackController
+class SlackController extends Controller
 {
     public function connectAction(ClientRegistry $clientRegistry)
     {
         return $clientRegistry
             ->getClient('slack')
-            ->redirect(['commands']      );
+            ->redirect(['commands']);
     }
 
     public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
     {
-        /** @var SlackClient $client */
-        $client = $clientRegistry->getClient('slack');
-
         try {
-            // the exact class depends on which provider you're using
-            /** @var SlackClient $user */
-            $user = $client->fetchUser();
+            $url = sprintf(
+                'https://slack.com/api/oauth.access?code=%s&client_id=%s&client_secret=%s&redirect_uri=%s',
+                $request->query->get('code'),
+                $this->getParameter('oauth_client_id'),
+                $this->getParameter('oauth_client_secret'),
+                $this->generateUrl('connect_slack_check', [], UrlGeneratorInterface::ABSOLUTE_URL)
+            );
 
-            // do something with all this new power!
-            // e.g. $name = $user->getFirstName();
-            var_dump($user); die;
-            // ...
+            $guzzleClient = new Client();
+            $response = $guzzleClient->request('GET', $url);
+
+            if(200 !== $response->getStatusCode()) {
+                return new Response('Something went wrong with access slack');
+            }
+
+            return new Response('ok. Everything should work now');
         } catch (IdentityProviderException $e) {
-            // something went wrong!
-            // probably you should return the reason to the user
-            var_dump($e->getMessage()); die;
+            return new Response('error', 500);
         }
     }
 }
